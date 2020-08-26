59c74d3e629ec134a4b700742caaa26b858c0186
wuxingjiang <762987783@qq.com>
2020-08-26 20:26:10 +0800

    https://github.com/wuxingijang/hy-admin.git

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'view-design/dist/styles/iview.css'
+import ViewUI from 'view-design'
+Vue.use(ViewUI)
 Vue.config.productionTip = false
 
 new Vue({
